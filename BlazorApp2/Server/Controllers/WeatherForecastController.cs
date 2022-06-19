bb83b08bfcecc975d8c8971153bc936c4f43de3c
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2022-06-19 13:04:13 +0900

    あ

--- BlazorApp2/Server/Controllers/WeatherForecastController.cs
+++ BlazorApp2/Server/Controllers/WeatherForecastController.cs
@@ -24,7 +24,7 @@ namespace BlazorApp2.Server.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
+                Date = DateTime.Now.AddDays(index).ToString(),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
